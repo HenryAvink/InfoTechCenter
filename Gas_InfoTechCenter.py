29b945edabc423cd11bbb995da2b13bf4d877a3f
Henry Avink <henryscomputerscience@gmail.com>
2022-02-15 13:37:51 -0500

    Fixed Comment

--- Gas_InfoTechCenter.py
+++ Gas_InfoTechCenter.py
@@ -1,5 +1,7 @@
 
-import random # Gas Level Functiondef gasLevelGauge():
+import random
+
+# Gas Level Function
 
 def gasLevelGauge():
     gasLevelList = ["Empty","Low","Quarter Tank","Half Tank","Three Quarter Tank","Full"]
